84204f23a3936aadeb88b18e86fb0f9732587f06
Selvam Murugan <smuruga5@jcp.com>
2019-08-13 16:28:56 +0530

    second table update

--- js/wp_racing_admin.js
+++ js/wp_racing_admin.js
@@ -17,23 +17,27 @@ jQuery(document).ready(function($) {
                     for (i = 0; i < tableRows.length; i++) { 
                         var currentRow = tableRows[i];
                         tableData[i] = {
-                            "PL": jQuery(currentRow).find('td:eq(0)').text(),
-                            "Name": jQuery(currentRow).find('td:eq(1)').text(),
-                            "Nr": jQuery(currentRow).find('td:eq(2)').text(),
-                            "Box": jQuery(currentRow).find('td:eq(3)').text(),
-                            "Abstand": jQuery(currentRow).find('td:eq(4)').text(),
-                            "Gewinn": jQuery(currentRow).find('td:eq(5)').text(),
-                            "Besitzer": jQuery(currentRow).find('td:eq(6)').text(),
-                            "Trainer": jQuery(currentRow).find('td:eq(7)').text(),
-                            "Reiter": jQuery(currentRow).find('td:eq(8)').text(),
-                            "Gew": jQuery(currentRow).find('td:eq(9)').text(),
-                            "Quote": jQuery(currentRow).find('td:eq(10)').text(),
+                            "N": jQuery(currentRow).find('td:eq(0)').text(),
+                            "Horse": jQuery(currentRow).find('td:eq(1)').text(),
+                            "Sire": jQuery(currentRow).find('td:eq(2)').text(),
+                            "Draw": jQuery(currentRow).find('td:eq(3)').text(),
+                            "Colors": jQuery(currentRow).find('td:eq(4)').text(),
+                            "Owner": jQuery(currentRow).find('td:eq(5)').text(),
+                            "Trainer": jQuery(currentRow).find('td:eq(6)').text(),
+                            "Jockey": jQuery(currentRow).find('td:eq(7)').text(),
+                            "Weight": jQuery(currentRow).find('td:eq(8)').text(),
+                            "Earnings": jQuery(currentRow).find('td:eq(9)').text(),
+                            "Form": jQuery(currentRow).find('td:eq(10)').text(),
+                            "Rating": jQuery(currentRow).find('td:eq(11)').text(),
+                            "Blink": jQuery(currentRow).find('td:eq(12)').text(),
+                            "Coupled": jQuery(currentRow).find('td:eq(13)').text(),
+                            "Claim": jQuery(currentRow).find('td:eq(14)').text(),
+                            "Breeders": jQuery(currentRow).find('td:eq(15)').text(),
                           };
                       }
 
                       const tableDom = tableData.map((data, idx) => {
-                          var PLId = data.PL.replace(/\./g,'');
-                            return ('<div><div class="field"><label for="race_id">Pl Id</label><input type="number" name="race_id_'+idx+'" id="race_id" value="'+PLId+'"/></div><div class="field"><label for="race_name">Name</label><input type="text" name="race_name_'+idx+'" id="race_name" value="'+data.Name+'"/></div><div class="field"><label for="race_nr">Nr</label> <input type="text" name="race_nr_'+idx+'" id="race_nr" value="'+data.Nr+'"/></div><div class="field"><label for="race_box">Box</label><input type="text" name="race_box_'+idx+'" id="race_box" value="'+data.Box+'"/></div><div class="field"><label for="race_abstand">Abstand</label><input type="text" name="race_abstand_'+idx+'" id="race_abstand" value="'+data.Abstand+'"/></div><div class="field"><label for="race_gewinn">Gewinn</label><input type="text" name="race_gewinn_'+idx+'" id="race_gewinn" value="'+data.Gewinn+'"/></div><div class="field"><label for="race_besitzer">Besitzer</label><input type="text" name="race_besitzer_'+idx+'" id="race_besitzer" value="'+data.Besitzer+'"/></div><div class="field"><label for="race_trainer">Trainer</label><input type="text" name="race_trainer_'+idx+'" id="race_trainer" value="'+data.Trainer+'"/></div><div class="field"><label for="race_reiter">Reiter</label><input type="text" name="race_reiter_'+idx+'" id="race_reiter" value="'+data.Reiter+'"/></div><div class="field"><label for="race_gew">Gew</label><input type="text" name="race_gew_'+idx+'" id="race_gew" value="'+data.Gew+'"/></div><div class="field"><label for="race_quote">Quote</label><input type="text" name="race_quote_'+idx+'" id="race_quote" value="'+data.Quote+'"/></div></div>');
+                            return ('<div><div class="field"><label for="race_id">N°</label><input type="number" name="race_id_'+idx+'" id="race_id" value="'+data.N+'"/></div><div class="field"><label for="horse_name">Horse Name</label><input type="text" name="horse_name_'+idx+'" id="horce_name" value="'+data.Horse+'"/></div><div class="field"><label for="race_sire">Sire/Dam</label> <input type="text" name="race_sire_'+idx+'" id="race_sire" value="'+data.Sire+'"/></div><div class="field"><label for="race_Draw">Draw</label><input type="text" name="race_draw_'+idx+'" id="race_Draw" value="'+data.Draw+'"/></div><div class="field"><label for="race_colors">Colors</label><input type="text" name="race_colors_'+idx+'" id="race_colors" value="'+data.Colors+'"/></div><div class="field"><label for="race_owner">Owner</label><input type="text" name="race_owner_'+idx+'" id="race_owner" value="'+data.Owner+'"/></div><div class="field"><label for="race_trainer">Trainer</label><input type="text" name="race_trainer_'+idx+'" id="race_trainer" value="'+data.Trainer+'"/></div><div class="field"><label for="race_jockey">Jockey</label><input type="text" name="race_jockey_'+idx+'" id="race_jockey" value="'+data.Jockey+'"/></div><div class="field"><label for="race_weight">Weight</label><input type="text" name="race_weight_'+idx+'" id="race_weight" value="'+data.Weight+'"/></div><div class="field"><label for="race_earnings">Earnings</label><input type="text" name="race_earnings_'+idx+'" id="race_earnings" value="'+data.Earnings+'"/></div><div class="field"><label for="race_form">Form</label><input type="text" name="race_form_'+idx+'" id="race_form" value="'+data.Form+'"/></div><div class="field"><label for="race_rating">Rating</label><input type="text" name="race_rating_'+idx+'" id="race_rating" value="'+data.Rating+'"/></div><div class="field"><label for="race_blink">Blink</label><input type="text" name="race_blink_'+idx+'" id="race_blink" value="'+data.Blink+'"/></div><div class="field"><label for="race_coupled">Coupled</label><input type="text" name="race_coupled_'+idx+'" id="race_coupled" value="'+data.Coupled+'"/></div><div class="field"><label for="race_claim">Claim</label><input type="text" name="race_claim_'+idx+'" id="race_claim" value="'+data.Claim+'"/></div><div class="field"><label for="race_breeders">Breeders</label><input type="text" name="race_breeders_'+idx+'" id="race_breeders" value="'+data.Breeders+'"/></div></div>');
                       });
                       tableDom.push('<input type="hidden" name="totalRows" value="'+tableData.length+'" />');
                       raceStatusTable.empty();
--- wp_racing_table_plugin.php
+++ wp_racing_table_plugin.php
@@ -3,10 +3,6 @@
 Plugin Name: WordPress Get Race Data
 Description: Creates an interfaces to get and manage race datas from other websites.
 Version:     1.0.0
-Author:      Selvam Murugan
-Author URI:  https://github.com/selvambe23
-License:     GPL2
-License URI: https://www.gnu.org/licenses/gpl-2.0.html
 */
 
 class wp_simple_location{
@@ -108,60 +104,85 @@ class wp_simple_location{
             <?php $totalRows = get_post_meta($post->ID,'totalRows',true);
             for($i=0; $i < $totalRows; $i++){
                 $race_id = get_post_meta($post->ID,'race_id_'.$i,true);
-                $race_name = get_post_meta($post->ID,'race_name_'.$i,true);
-                $race_nr = get_post_meta($post->ID,'race_nr_'.$i,true);
-                $race_box = get_post_meta($post->ID,'race_box_'.$i,true);
-                $race_abstand = get_post_meta($post->ID,'race_abstand_'.$i,true);
-                $race_gewinn = get_post_meta($post->ID,'race_gewinn_'.$i,true);
-                $race_besitzer = get_post_meta($post->ID,'race_besitzer_'.$i,true);
+                $horse_name = get_post_meta($post->ID,'horse_name_'.$i,true);
+                $race_sire = get_post_meta($post->ID,'race_sire_'.$i,true);
+                $race_draw = get_post_meta($post->ID,'race_draw_'.$i,true);
+                $race_colors = get_post_meta($post->ID,'race_colors_'.$i,true);
+                $race_owner = get_post_meta($post->ID,'race_owner_'.$i,true);
                 $race_trainer = get_post_meta($post->ID,'race_trainer_'.$i,true);
-                $race_reiter = get_post_meta($post->ID,'race_reiter_'.$i,true);
-                $race_gew = get_post_meta($post->ID,'race_gew_'.$i,true);
-                $race_quote = get_post_meta($post->ID,'race_quote_'.$i,true);
+                $race_jockey = get_post_meta($post->ID,'race_jockey_'.$i,true);
+                $race_weight = get_post_meta($post->ID,'race_weight_'.$i,true);
+                $race_earnings = get_post_meta($post->ID,'race_earnings_'.$i,true);
+                $race_form = get_post_meta($post->ID,'race_form_'.$i,true);
+                $race_rating = get_post_meta($post->ID,'race_rating_'.$i,true);
+                $race_blink = get_post_meta($post->ID,'race_blink_'.$i,true);
+                $race_coupled = get_post_meta($post->ID,'race_coupled_'.$i,true);
+                $race_claim = get_post_meta($post->ID,'race_claim_'.$i,true);
+                $race_breeders = get_post_meta($post->ID,'race_breeders_'.$i,true);
             ?>
                 <div class="field">
-                    <label for="race_id_<?php echo $i;?>">Pl Id</label>
+                    <label for="race_id_<?php echo $i;?>">N°</label>
                     <input type="number" name="race_id_<?php echo $i;?>" id="race_id_<?php echo $i;?>" value="<?php echo $race_id;?>"/>
                 </div>
                 <div class="field">
-                    <label for="race_name_<?php echo $i;?>">Name</label>
-                    <input type="text" name="race_name_<?php echo $i;?>" id="race_name_<?php echo $i;?>" value="<?php echo $race_name;?>"/>
+                    <label for="horse_name_<?php echo $i;?>">Horse</label>
+                    <input type="text" name="horse_name_<?php echo $i;?>" id="horse_name_<?php echo $i;?>" value="<?php echo $horse_name;?>"/>
                 </div>
                 <div class="field">
-                    <label for="race_nr_<?php echo $i;?>">Nr</label>
-                    <input type="text" name="race_nr_<?php echo $i;?>" id="race_nr_<?php echo $i;?>" value="<?php echo $race_nr;?>"/>
+                    <label for="race_sire_<?php echo $i;?>">Sire/Dam</label>
+                    <input type="text" name="race_sire_<?php echo $i;?>" id="race_sire_<?php echo $i;?>" value="<?php echo $race_sire;?>"/>
                 </div>
                 <div class="field">
-                    <label for="race_box_<?php echo $i;?>">Box</label>
-                    <input type="text" name="race_box_<?php echo $i;?>" id="race_box_<?php echo $i;?>" value="<?php echo $race_box;?>"/>
+                    <label for="race_draw_<?php echo $i;?>">Draw</label>
+                    <input type="text" name="race_draw_<?php echo $i;?>" id="race_draw_<?php echo $i;?>" value="<?php echo $race_draw;?>"/>
                 </div>
                 <div class="field">
-                    <label for="race_abstand_<?php echo $i;?>">Abstand</label>
-                    <input type="text" name="race_abstand_<?php echo $i;?>" id="race_abstand_<?php echo $i;?>" value="<?php echo $race_abstand;?>"/>
+                    <label for="race_colors_<?php echo $i;?>">Colors</label>
+                    <input type="text" name="race_colors_<?php echo $i;?>" id="race_colors_<?php echo $i;?>" value="<?php echo $race_colors;?>"/>
                 </div>
                 <div class="field">
-                    <label for="race_gewinn_<?php echo $i;?>">Gewinn</label>
-                    <input type="text" name="race_gewinn_<?php echo $i;?>" id="race_gewinn_<?php echo $i;?>" value="<?php echo $race_gewinn;?>"/>
-                </div>
-                <div class="field">
-                    <label for="race_besitzer_<?php echo $i;?>">Besitzer</label>
-                    <input type="text" name="race_besitzer_<?php echo $i;?>" id="race_besitzer_<?php echo $i;?>" value="<?php echo $race_besitzer;?>"/>
+                    <label for="race_owner_<?php echo $i;?>">Owner</label>
+                    <input type="text" name="race_owner_<?php echo $i;?>" id="race_owner_<?php echo $i;?>" value="<?php echo $race_owner;?>"/>
                 </div>
                 <div class="field">
                     <label for="race_trainer_<?php echo $i;?>">Trainer</label>
                     <input type="text" name="race_trainer_<?php echo $i;?>" id="race_trainer_<?php echo $i;?>" value="<?php echo $race_trainer;?>"/>
                 </div>
                 <div class="field">
-                    <label for="race_reiter_<?php echo $i;?>">Reiter</label>
-                    <input type="text" name="race_reiter_<?php echo $i;?>" id="race_reiter_<?php echo $i;?>" value="<?php echo $race_reiter;?>"/>
+                    <label for="race_jockey_<?php echo $i;?>">Jockey</label>
+                    <input type="text" name="race_jockey_<?php echo $i;?>" id="race_jockey_<?php echo $i;?>" value="<?php echo $race_jockey;?>"/>
                 </div>
                 <div class="field">
-                    <label for="race_gew_<?php echo $i;?>">Gew</label>
-                    <input type="text" name="race_gew_<?php echo $i;?>" id="race_gew_<?php echo $i;?>" value="<?php echo $race_gew;?>"/>
+                    <label for="race_weight_<?php echo $i;?>">Weight</label>
+                    <input type="text" name="race_weight_<?php echo $i;?>" id="race_weight_<?php echo $i;?>" value="<?php echo $race_weight;?>"/>
                 </div>
                 <div class="field">
-                    <label for="race_quote_<?php echo $i;?>">Quote</label>
-                    <input type="text" name="race_quote_<?php echo $i;?>" id="race_quote_<?php echo $i;?>" value="<?php echo $race_quote;?>"/>
+                    <label for="race_earnings_<?php echo $i;?>">Earnings</label>
+                    <input type="text" name="race_earnings_<?php echo $i;?>" id="race_earnings_<?php echo $i;?>" value="<?php echo $race_earnings;?>"/>
+                </div>
+                <div class="field">
+                    <label for="race_form_<?php echo $i;?>">Form</label>
+                    <input type="text" name="race_form_<?php echo $i;?>" id="race_form_<?php echo $i;?>" value="<?php echo $race_form;?>"/>
+                </div>
+                <div class="field">
+                    <label for="race_rating_<?php echo $i;?>">Rating</label>
+                    <input type="text" name="race_rating_<?php echo $i;?>" id="race_rating_<?php echo $i;?>" value="<?php echo $race_rating;?>"/>
+                </div>
+                <div class="field">
+                    <label for="race_blink_<?php echo $i;?>">Blink</label>
+                    <input type="text" name="race_blink_<?php echo $i;?>" id="race_blink_<?php echo $i;?>" value="<?php echo $race_blink;?>"/>
+                </div>
+                <div class="field">
+                    <label for="race_coupled_<?php echo $i;?>">Coupled</label>
+                    <input type="text" name="race_coupled_<?php echo $i;?>" id="race_coupled_<?php echo $i;?>" value="<?php echo $race_coupled;?>"/>
+                </div>
+                <div class="field">
+                    <label for="race_claim_<?php echo $i;?>">Claim</label>
+                    <input type="text" name="race_claim_<?php echo $i;?>" id="race_claim_<?php echo $i;?>" value="<?php echo $race_claim;?>"/>
+                </div>
+                <div class="field">
+                    <label for="race_breeders_<?php echo $i;?>">Breeders</label>
+                    <input type="text" name="race_breeders_<?php echo $i;?>" id="race_breeders_<?php echo $i;?>" value="<?php echo $race_breeders;?>"/>
                 </div>
             <?php } ?>
             <input type="hidden" name="totalRows" value="<?php echo $totalRows; ?>"/>
@@ -191,29 +212,39 @@ class wp_simple_location{
         for($i=0; $i < $_POST['totalRows']; $i++){
             //Reading all table Row values
             $race_id = isset($_POST['race_id_'.$i]) ? sanitize_text_field($_POST['race_id_'.$i]) : '';
-            $race_name = isset($_POST['race_name_'.$i]) ? sanitize_text_field($_POST['race_name_'.$i]) : '';
-            $race_nr = isset($_POST['race_nr_'.$i]) ? sanitize_text_field($_POST['race_nr_'.$i]) : '';
-            $race_box = isset($_POST['race_box_'.$i]) ? sanitize_text_field($_POST['race_box_'.$i]) : '';
-            $race_abstand = isset($_POST['race_abstand_'.$i]) ? sanitize_text_field($_POST['race_abstand_'.$i]) : '';
-            $race_gewinn = isset($_POST['race_gewinn_'.$i]) ? sanitize_text_field($_POST['race_gewinn_'.$i]) : '';
-            $race_besitzer = isset($_POST['race_besitzer_'.$i]) ? sanitize_text_field($_POST['race_besitzer_'.$i]) : '';
+            $horse_name = isset($_POST['horse_name_'.$i]) ? sanitize_text_field($_POST['horse_name_'.$i]) : '';
+            $race_sire = isset($_POST['race_sire_'.$i]) ? sanitize_text_field($_POST['race_sire_'.$i]) : '';
+            $race_draw = isset($_POST['race_draw_'.$i]) ? sanitize_text_field($_POST['race_draw_'.$i]) : '';
+            $race_colors = isset($_POST['race_colors_'.$i]) ? sanitize_text_field($_POST['race_colors_'.$i]) : '';
+            $race_owner = isset($_POST['race_owner_'.$i]) ? sanitize_text_field($_POST['race_owner_'.$i]) : '';
             $race_trainer = isset($_POST['race_trainer_'.$i]) ? sanitize_text_field($_POST['race_trainer_'.$i]) : '';
-            $race_reiter = isset($_POST['race_reiter_'.$i]) ? sanitize_text_field($_POST['race_reiter_'.$i]) : '';
-            $race_gew = isset($_POST['race_gew_'.$i]) ? sanitize_text_field($_POST['race_gew_'.$i]) : '';
-            $race_quote = isset($_POST['race_quote_'.$i]) ? sanitize_text_field($_POST['race_quote_'.$i]) : '';
+            $race_jockey = isset($_POST['race_jockey_'.$i]) ? sanitize_text_field($_POST['race_jockey_'.$i]) : '';
+            $race_weight = isset($_POST['race_weight_'.$i]) ? sanitize_text_field($_POST['race_weight_'.$i]) : '';
+            $race_earnings = isset($_POST['race_earnings_'.$i]) ? sanitize_text_field($_POST['race_earnings_'.$i]) : '';
+            $race_form = isset($_POST['race_form_'.$i]) ? sanitize_text_field($_POST['race_form_'.$i]) : '';
+            $race_rating = isset($_POST['race_rating_'.$i]) ? sanitize_text_field($_POST['race_rating_'.$i]) : '';
+            $race_blink = isset($_POST['race_blink_'.$i]) ? sanitize_text_field($_POST['race_blink_'.$i]) : '';
+            $race_coupled = isset($_POST['race_coupled_'.$i]) ? sanitize_text_field($_POST['race_coupled_'.$i]) : '';
+            $race_claim = isset($_POST['race_claim_'.$i]) ? sanitize_text_field($_POST['race_claim_'.$i]) : '';
+            $race_breeders = isset($_POST['race_breeders_'.$i]) ? sanitize_text_field($_POST['race_breeders_'.$i]) : '';
             
             //update all the table column fields
             update_post_meta($post_id, 'race_id_'.$i, $race_id);
-            update_post_meta($post_id, 'race_name_'.$i, $race_name);
-            update_post_meta($post_id, 'race_nr_'.$i, $race_nr);
-            update_post_meta($post_id, 'race_box_'.$i, $race_box);
-            update_post_meta($post_id, 'race_abstand_'.$i, $race_abstand);
-            update_post_meta($post_id, 'race_gewinn_'.$i, $race_gewinn);
-            update_post_meta($post_id, 'race_besitzer_'.$i, $race_besitzer);
+            update_post_meta($post_id, 'horse_name_'.$i, $horse_name);
+            update_post_meta($post_id, 'race_sire_'.$i, $race_sire);
+            update_post_meta($post_id, 'race_draw_'.$i, $race_draw);
+            update_post_meta($post_id, 'race_colors_'.$i, $race_colors);
+            update_post_meta($post_id, 'race_owner_'.$i, $race_owner);
             update_post_meta($post_id, 'race_trainer_'.$i, $race_trainer);
-            update_post_meta($post_id, 'race_reiter_'.$i, $race_reiter);
-            update_post_meta($post_id, 'race_gew_'.$i, $race_gew);
-            update_post_meta($post_id, 'race_quote_'.$i, $race_quote);
+            update_post_meta($post_id, 'race_jockey_'.$i, $race_jockey);
+            update_post_meta($post_id, 'race_weight_'.$i, $race_weight);
+            update_post_meta($post_id, 'race_earnings_'.$i, $race_earnings);
+            update_post_meta($post_id, 'race_form_'.$i, $race_form);
+            update_post_meta($post_id, 'race_rating_'.$i, $race_rating);
+            update_post_meta($post_id, 'race_blink_'.$i, $race_blink);
+            update_post_meta($post_id, 'race_coupled_'.$i, $race_coupled);
+            update_post_meta($post_id, 'race_claim_'.$i, $race_claim);
+            update_post_meta($post_id, 'race_breeders_'.$i, $race_breeders);
         }
         update_post_meta($post_id, 'totalRows', $_POST['totalRows']);
         update_post_meta($post_id, 'fetch_url', $_POST['fetch_url']);
@@ -222,8 +253,8 @@ class wp_simple_location{
 	
 	//enqueus scripts and stles on the back end
 	public function enqueue_admin_scripts_and_styles(){
-		wp_enqueue_style('wp_race_tables_admin_styles', plugin_dir_url(__FILE__) . '/css/wp_racing_admin_styles.css');
-		wp_enqueue_script('wp_race_tables_admin_scripts', plugin_dir_url(__FILE__) . '/js/wp_racing_admin.js');
+		wp_enqueue_style('wp_race_tables_admin_styles', plugin_dir_url(__FILE__) . 'css/wp_racing_admin_styles.css');
+		wp_enqueue_script('wp_race_tables_admin_scripts', plugin_dir_url(__FILE__) . 'js/wp_racing_admin.js');
     }
     
     //triggered on activation of the plugin (called only once)
